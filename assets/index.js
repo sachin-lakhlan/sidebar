379b791aa98fe62d3647f004d8b69445d3e50e5e
sachin-lakhlan <lakhlansachin0@gmail.com>
2024-12-31 12:30:36 +0530

    side-bar

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+function navopen() {
+    document.getElementById("side-bar-main").style.width = "286px";
+    document.getElementById("layer").style.display = "block";
+}
+
+function navclose() {
+    document.getElementById("side-bar-main").style.width = "0";
+    document.getElementById("layer").style.display = "none";
+
+}
